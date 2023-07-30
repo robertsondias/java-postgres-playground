b41e79507fc34ea74c777854489c9da4adf94765
Robertson Dias <robertsondias@gmail.com>
2023-07-30 15:19:58 +0000

    Modificado AppOperadoresRelacionais Sinal de =

--- src/main/java/com/example/AppOperadoresRelacionais.java
+++ src/main/java/com/example/AppOperadoresRelacionais.java
@@ -3,17 +3,21 @@ package com.example;
 public class AppOperadoresRelacionais {
     public static void main(String[] args) {
         var cliente1 = new Cliente();
-        cliente1.setAnoNascimento(2002);
+        cliente1.setAnoNascimento(2003);
 
         var cliente2 = new Cliente();
         cliente2.setAnoNascimento(2003);
 
-        if (cliente1.getAnoNascimento() > cliente2.getAnoNascimento()){
+        boolean Cliente1MaisJovem = cliente1.getAnoNascimento() > cliente2.getAnoNascimento();
+        boolean Cliente1MaisVelho = cliente1.getAnoNascimento() < cliente2.getAnoNascimento();
+        
+        if (Cliente1MaisJovem) {
             System.out.println("OS cliente 1 é mais novo que o cliente 2");  
         } 
-        else if (cliente1.getAnoNascimento() < cliente2.getAnoNascimento()) {
+        else if (Cliente1MaisVelho) {    
             System.out.println("O cliente 1 é mais velho que o 2.");         
         }
-        else System.out.println("OS clientes têm a mesma idade.");
+        else    
+            System.out.println("OS clientes têm a mesma idade.");
     }
 }
