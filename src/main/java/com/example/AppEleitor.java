f211225996c41627ef2cd98a559860d599233bf4
Robertson Dias <robertsondias@gmail.com>
2023-07-31 00:17:15 +0000

    Adicionado AppEleitor

---
+++ src/main/java/com/example/AppEleitor.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import java.time.LocalDate;
+
+public class AppEleitor {
+    public static void main(String[] args) {
+        var cidadao = new Cidadao();
+        cidadao.setDataNascimento(LocalDate.of(1932, 9, 29));
+        System.out.println(cidadao.idade());
+        System.out.println(cidadao.eleitor());
+    }
+}
