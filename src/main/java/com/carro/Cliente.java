4d80174fae96c0a4f7d1597db3956190b2b91f19
Robertson Dias <robertsondias@gmail.com>
2023-08-02 15:25:16 +0000

    Add AppClasse.java aula  9

--- src/main/java/com/carro/Cliente.java
+++ src/main/java/com/carro/Cliente.java
@@ -1,10 +1,61 @@
 package com.carro;
 
 public class Cliente {
+    private String cpf;
+    private String nome;
+    private String cidade;
     private double renda;
     private char sexo;
     private int anoNascimento;
-    
+    private boolean especial;
+        
+    public Cliente(){
+        System.out.println(" Criando um construtor sem parâmentro: ");
+        double aleatorio = Math.random();
+        if (aleatorio > 0.5 )
+            especial = true;
+
+    }
+    public Cliente(double renda, char sexo){
+        this();
+        System.out.println(" Criando um construtor com parâmentros: ");
+        setRenda(renda);
+        this.sexo = sexo;
+    }
+
+    public String getCidade() {
+        return cidade;
+        
+    }
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        if (nome.isBlank())
+            System.out.println("O nome é obrigatório");
+        else this.nome = nome.toUpperCase().trim();
+    }
+ 
+    public boolean isEspecial() {
+        return especial;
+    }
+
+    public void setEspecial(boolean especial) {
+        this.especial = especial;
+    }
+
+
 
     public char getSexo() {
         return sexo;
@@ -31,7 +82,7 @@ public class Cliente {
     public void setRenda(double renda) {
         if (renda >= 0)
             this.renda = renda;
-        else System.out.println("A renda deve ser maior que zero");
+        else System.out.println("A renda deve ser maior ou igual a zero");
         }
     
 }
